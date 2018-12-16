72e509d4ee82c3e4c557d21e1e7c2fd8561a4832
Dmytro Lukianenko <dmi3yy@gmail.com>
2018-12-16 15:09:00 +0300

    Update snippet.listindexer.php

--- assets/snippets/listindexer/snippet.listindexer.php
+++ assets/snippets/listindexer/snippet.listindexer.php
@@ -445,7 +445,7 @@ if ($mode == 'short') {
 }
 
 while (($recentCounter < $recentCounterLimit) && $rsRecent && ($recentLimit > 0)){
-  mysql_data_seek($rsRecent,$recentCounter);
+  $modx-db->dataSeek($rsRecent,$recentCounter);
   $recentRecord = $modx->fetchRow($rsRecent);
   $output .= '<li>';
   // Link to page
